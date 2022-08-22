166af48cd1f51e697e4a95e737c0e483eee0f162
AytacSafarova <aytaccseferr@gmail.com>
2022-08-22 22:38:15 +0400

    front&back

--- main.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector(".fa-bars")
-const left_container = document.querySelector(".left-container")
-
-menu.addEventListener('click',openslidebar)
-function openslidebar(){
-   left_container.style.display="block"
-
-}
